fa12c1f28577c7d48b8b765a83b63c4e8c40436b
Stela Capsa <stela.capsa@gmail.com>
2020-03-30 04:39:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
+
 class App < Sinatra::Base
   get '/' do
     erb :index
   end
 
-  post '/' do
-    text_from_user = params[:user_text]
-
-    erb :results
+post '/results' do
+ @analyzed_text = TextAnalyzer.new(params[:user_text])
+ 
+  erb :results
   end
+  
+  # post '/results' do 
+  #   erb :results
+  # end 
+ 
 end
